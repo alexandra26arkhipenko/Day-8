4932a471063bbd802a386303be6517431a2efc1d
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-11-06 16:13:55 +0300

    8

---
+++ NET.W.2017.Arkhipenko.8/BookShop/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Books;
+
+namespace BookShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Book> books = new List<Book>();
+          
+            books.Add(new Book(111, "Булгаков", "Мастер и Маргарита", "OOO МоскваПечать", 1998, 15, 740));
+            books.Add(new Book(112, "Достоевский", "Преступление и наказание", "OOO МоскваПечать", 1995, 12, 520));
+            books.Add(new Book(113, "Толстой", "Война и мир", "OOO МоскваПечать", 2001, 25, 1740));
+            books.Add(new Book(114, "Булгаков", "Мастер и Маргарита111", "OOO МоскваПечать", 1998, 15, 740));
+            books.Add(new Book(115, "Достоевский11111", "Преступление и наказание", "OOO МоскваПечать", 1995, 12, 520));
+            books.Add(new Book(116, "Толстой11111", "Война и мир", "OOO МоскваПечать", 2001, 25, 1740));
+            
+            IBookListService  bookListService = new BookListService();
+            
+            bookListService.AddBook(books[0]);
+            bookListService.AddBook(books[1]);
+            bookListService.AddBook(books[2]);
+            bookListService.AddBook(books[3]);
+            bookListService.AddBook(books[4]);
+            bookListService.AddBook(books[5]);
+            
+            Print(bookListService.GetBookList());
+
+            Book bookFind = bookListService.FindBook(115);
+            Console.WriteLine(bookFind.ToString());
+            Console.WriteLine(new string('-', 20));
+
+            bookListService.RemoveBook(116);
+            Print(bookListService.GetBookList());
+            
+            foreach (var book in bookListService.SortByName(books))
+            {
+                Console.WriteLine(book.ToString());
+            }
+            Console.ReadKey();
+        }
+
+        private static void Print(List<Book> books)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine(book.ToString());
+            }
+            Console.WriteLine(new string('-', 20));
+        }
+    }
+}
---
+++ NET.W.2017.Arkhipenko.8/Books/Book.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Books
+{
+   
+    public class Book : IEquatable<Book>, IComparable
+    {
+        #region private properties 
+        public int Isbn { get;  }
+        public string Author { get;  }
+        public string Name { get; }
+        public string Publish  { get;  }
+        public int Year { get;  }
+        public double Price { get; }
+        public int Pages { get; }
+        #endregion
+
+        #region public
+        public Book(int isbn, string author, string name, string publish, int year, double price, int pages)
+        {
+            Isbn = isbn;
+            Author = author;
+            Name = name;
+            Publish = publish;
+            Year = year;
+            Price = price;
+            Pages = pages;
+        }
+
+        
+
+        #region IEquatable methods
+
+        public bool Equals(Book book)
+        {
+            if (book == null) return false;
+
+            return Isbn == book.Isbn && Author == book.Author && Name == book.Name
+                   && Publish == book.Publish && Year == book.Year && Pages == book.Pages;
+        }
+
+        public static bool operator ==(Book lhs, Book rhs)
+        {
+            if (ReferenceEquals(lhs, rhs)) return true;
+            if (ReferenceEquals(lhs, null)) return false;
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(Book lhs, Book rhs)
+        {
+            return !(lhs == rhs);
+        }
+        #endregion
+        #region override
+
+        public override bool Equals(object bookEq)
+        {
+            var book = (Book) bookEq;
+            if (book == null)
+            {
+                return false;
+            }
+
+            return Isbn == book.Isbn && Author == book.Author && Name == book.Name
+                   && Publish == book.Publish && Year == book.Year && Pages == book.Pages;
+        }
+
+        public override int GetHashCode()
+        {
+            return Pages;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Book:\"{0}\" {1}, {2} pages \nPublishing company {3}, {4} y.," +
+                                 " ISBN: {5}\n Price: {6} y.e.", Name, Author,Pages, Publish, Year, Isbn, Price);
+        }
+        #endregion
+
+        public int CompareTo(object bookObj)
+        {
+            if (ReferenceEquals(bookObj, null)) return 1;
+            Book book = (Book) bookObj;
+            return Name.CompareTo(book.Name);
+        }
+        #endregion
+    }
+
+}
